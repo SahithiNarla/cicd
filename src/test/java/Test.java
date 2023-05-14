4031daae43ddc5f71cfeb774855664cdcb5d45f5
SahithiNarla <130640469+SahithiNarla@users.noreply.github.com>
2023-05-14 10:52:49 +0530

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,65 +1,75 @@
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class ArithmeticTest {
-
-@BeforeClass
-
-public static void setUpClass() {
-
-// This block is executed before all the methods of test
-
+public class CalculatorTest {
+     @Test
+     public void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluate("1+2+3");
+        assertEquals(6, sum);
+     }
 }
 
-@Before
 
-public void setUp() {
 
-// This block is executed before each method of test
 
-}
 
-@Test
 
-public void testProduct() {
 
-// This block is executed for testing product method of Arithmetic class
 
-Arithmetic arithmetic = new Arithmetic();
 
-int number1 = 100;
 
-int number2 = 5;
 
-int actualvalue = arithmetic.product(number1, number2);
 
-int expectedvalue = 500;
 
-assertEquals(expectedvalue, actualvalue);
 
-}
 
-@Test
 
-public void testDivision() {
 
-// This block is executed for testing division method of Arithmetic class
 
-}
 
-@After
 
-public void tearDown() {
 
-// This block is executed after each method of test
 
-}
 
-@AfterClass
 
-public static void tearDownClass() {
 
-// This block is executed after all the methods of test
 
-}
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
