292af6a01a357994ad57d3bb39672b3000a89f39
SahithiNarla <130640469+SahithiNarla@users.noreply.github.com>
2023-05-14 09:04:18 +0530

    Update app.java

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,1 +1,15 @@
+public class Arithmetic {
 
+public int product(int number1, int number2) {
+
+return number1 * number2;
+
+}
+
+public int division(int number1, int number2) {
+
+return number1 / number2;
+
+}
+
+}
